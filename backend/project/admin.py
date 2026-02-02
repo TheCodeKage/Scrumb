779c19ed1b0f195a024372845ef01a507bb58580
naman <nam.agr96@gmail.com>
2026-02-02 15:52:15 +0530

    Week One over, Completed: Project CRUD, Task Detail, Skill and UserSkill models

--- backend/project/admin.py
+++ backend/project/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from project.models import Idea, Project, Feature
+from .models import Idea, Project, Feature, Task
 
 # Register your models here.
 admin.site.register(Idea)
 admin.site.register(Project)
 admin.site.register(Feature)
+admin.site.register(Task)
