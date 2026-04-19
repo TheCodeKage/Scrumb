74795eb92aeaba9e0da876a3f483b0cf277be240
Naman Agrawal <nam.agr96@gmail.com>
2026-04-19 09:20:03 +0530

    removed github logic (gonna re-implement, last one was messy, also half the logic was re-assigned to Goapple)

--- backend/Github/auth.py
+++ backend/Github/auth.py
@@ -30,7 +30,11 @@ def get_installation_token(installation_id):
     }
 
     response = requests.post(url, headers=headers)
-    print(response.text)
+
+    print("JWT:", get_token())
+    print("Status:", response.status_code)
+    print("Response:", response.text)
+
     response.raise_for_status()
 
     return response.json()["token"]
